1483559325092d5957abe279432f648465a9a90d
AmauryFaveriel <mrfvrl@gmail.com>
2017-12-26 19:50:56 +0100

    modified all

--- js/script.js
+++ js/script.js
@@ -1,63 +1,66 @@
-var strbtn=document.querySelector(".startButton");
-  var date=document.querySelector(".date input");
-  date.addEventListener("change", function(){
-    strbtn.style.display="block";
-  })
-strbtn.addEventListener("click", function(){
-  var containerAll=document.querySelector(".containerAll");
-  containerAll.style.display="block";
-  var container=document.querySelector(".calendarContainer");
-  container.innerHTML="";
-  for(i=0;i<53;i++){
-    container.innerHTML+='<div class="column"></div>';
+var strbtn = document.querySelector(".startButton");
+var date = document.querySelector(".date input");
+date.addEventListener("change", function() {
+  strbtn.style.display = "block";
+})
+strbtn.addEventListener("click", function() {
+  var containerAll = document.querySelector(".containerAll");
+  containerAll.style.display = "block";
+  var container = document.querySelector(".calendarContainer");
+  container.innerHTML = "";
+  for (i = 0; i < 53; i++) {
+    container.innerHTML += '<div class="column"></div>';
   }
-  var column=container.querySelectorAll(".column");
-  for(i=0;i<column.length;i++){
-    for(a=0;a<7;a++){
-      column[i].innerHTML+='<div class="dayEmpty"></div>';
+  var column = container.querySelectorAll(".column");
+  for (i = 0; i < column.length; i++) {
+    for (a = 0; a < 7; a++) {
+      column[i].innerHTML += '<div class="dayEmpty"></div>';
     }
   }
 
-  var start=moment(date.value).day()%7;
-  var end=365+start;
+  var start = moment(date.value).day() % 7;
+  var end = 365 + start;
 
-  var daysEmpty=container.querySelectorAll(".dayEmpty");
-  for(let i=start;i<end;i++){
-    daysEmpty[i].className="day NotClicked";
-    daysEmpty[i].title=(moment(date.value).add((i-start), 'day').format("dddd Do MMMM YYYY"));
+  var daysEmpty = container.querySelectorAll(".dayEmpty");
+  for (let i = start; i < end; i++) {
+    daysEmpty[i].className = "day NotClicked";
+    daysEmpty[i].title = (moment(date.value).add((i - start), 'day').format("dddd Do MMMM YYYY"));
+    daysEmpty[i].id = (moment(date.value).add((i - start), 'day').format("MM/DD/YYYY"));
   }
 
-  var days=container.querySelectorAll(".day");
-  for(let i=0;i<days.length;i++){
-    days[i].addEventListener("click", function(){
-      if(days[i].classList.contains("NotClicked")===true){
-        days[i].className="day Clicked";
-      }
-      else{
-        days[i].className="day NotClicked";
+  var days = container.querySelectorAll(".day");
+  for (let i = 0; i < days.length; i++) {
+    days[i].addEventListener("click", function() {
+      if (days[i].classList.contains("NotClicked") === true) {
+        days[i].className = "day Clicked";
+      } else {
+        days[i].className = "day NotClicked";
       }
     })
   }
 
-  var endbtn=document.querySelector(".endButton");
-  endbtn.addEventListener("click", function(){
-    var textContainer=document.querySelector(".textContainer");
-    var daysClicked=container.querySelectorAll(".Clicked");
-    textContainer.innerHTML='<h1 class="textTitle">Dates auxquelles faire un commit</h1>'+
-    '<ul class="listDate"></ul>';
-    var list=textContainer.querySelector(".listDate");
-    for(let i=0;i<daysClicked.length;i++){
-      list.innerHTML+='<li class="listDateItem">'+daysClicked[i].title+'</li>';
+  var endbtn = document.querySelector(".endButton");
+  endbtn.addEventListener("click", function() {
+    var textContainer = document.querySelector(".textContainer");
+    var listID = document.querySelector(".listIDContainer");
+    var daysClicked = container.querySelectorAll(".Clicked");
+    textContainer.innerHTML = '<h1 class="textTitle">Dates auxquelles faire un commit :</h1>' +
+      '<ul class="listDate"></ul>';
+      listID.innerHTML = '<h1 class="listIDItem">Format tableur :</h1>';
+    var list = textContainer.querySelector(".listDate");
+    for (let i = 0; i < daysClicked.length; i++) {
+      list.innerHTML += '<li class="listDateItem">' + daysClicked[i].title + '</li>';
+      listID.innerHTML += '<li class="listIDTitle">' + daysClicked[i].id + '</li>';
     }
   })
 
-  var rstbtn=document.querySelector(".resetButton");
-  rstbtn.addEventListener("click", function(){
-    var textContainer=document.querySelector(".textContainer");
-    textContainer.innerHTML="";
-    var daysClicked=container.querySelectorAll(".Clicked");
-    for(let i=0;i<daysClicked.length;i++){
-      daysClicked[i].className="day NotClicked";
+  var rstbtn = document.querySelector(".resetButton");
+  rstbtn.addEventListener("click", function() {
+    var textContainer = document.querySelector(".textContainer");
+    textContainer.innerHTML = "";
+    var daysClicked = container.querySelectorAll(".Clicked");
+    for (let i = 0; i < daysClicked.length; i++) {
+      daysClicked[i].className = "day NotClicked";
     }
   })
 })
